41827fa9cba58cf0e486647449e42161ae89a723
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-12-15 16:55:27 +0500

    'commit-6'

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../ui/dashboard/Navbar/Navbar'
 import Sidebar from '../ui/dashboard/Sidebar/Sidebar'
 import styles from '@/app/ui/dashboard/dashboard.module.css'
+import Footer from '../ui/dashboard/footer/Footer'
 
 export default function Layout ({ children }) {
   return (
@@ -12,6 +13,7 @@ export default function Layout ({ children }) {
         <div className={styles.content}>
           <Navbar />
           {children}
+          <Footer />
         </div>
       </div>
     </>
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,13 +1,76 @@
 
+import { deleteUser } from "@/lib/actions";
+import { fetchUsers } from "@/lib/data";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 
+export default function Page ({}) {
+  const users = []
 
-
-
-export default function Page({}) {
-
-
-
-    return(<>
-    </>)
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <Search placeholder='Search for a user...' />
+          <Link href='/dashboard/users/add'>
+            <button className={styles.addButton}>Add New</button>
+          </Link>
+        </div>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Created At</td>
+              <td>Role</td>
+              <td>Status</td>
+              <td>Action</td>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map(user => (
+              <tr key={user?.id}>
+                <td>
+                  <div className={styles.user}>
+                    <Image
+                      src={user.img || '/noavatar.png'}
+                      alt=''
+                      width={40}
+                      height={40}
+                      className={styles.userImage}
+                    />
+                    {user.username}
+                  </div>
+                </td>
+                <td>{user?.email}</td>
+                <td>{user?.createdAt?.toString().slice(4, 16)}</td>
+                <td>{user?.isAdmin ? 'Admin' : 'Client'}</td>
+                <td>{user?.isActive ? 'active' : 'passive'}</td>
+                <td>
+                  <div className={styles.buttons}>
+                    <Link href={`/dashboard/users/abc`}>
+                      <button className={`${styles.button} ${styles.view}`}>
+                        View
+                      </button>
+                    </Link>
+                    <form action={deleteUser}>
+                      <input type='hidden' name='id' value={123} />
+                      <button className={`${styles.button} ${styles.delete}`}>
+                        Delete
+                      </button>
+                    </form>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination count={1} />
+      </div>
+    </>
+  )
 }
